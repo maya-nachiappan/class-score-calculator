ec56499a018be74abc447b13bbf2eddca20eb48b
maya-nachiappan <maya.n102304@gmail.com>
2024-10-02 10:29:10 -0700

    Initial Commit

---
+++ class score calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  class score calculator
+//
+//  Created by maya nachiappan on 10/2/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
